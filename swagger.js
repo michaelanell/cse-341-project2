0268bcd82010f3672a7b9a4d9f58e53bc7e7bce6
michaelanell <mickimaree@gmail.com>
2023-11-30 17:23:34 -0700

    remove https:// from host

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
       title: 'My API',
       description: 'Customers API',
     },
-    host: 'https://cse341-project2-z5y7.onrender.com',
-    schemes: ['https'],
+    host: 'cse341-project2-z5y7.onrender.com',
+    schemes: ['https','http'],
   };
 
   const outputFile = './swagger.json';
